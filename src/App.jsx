42c53081298e4d1f3b3c846df173ee69da146081
stocaimaza <stocaimaza@hotmail.com>
2023-05-08 12:01:22 -0300

    tengo hambre

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 //import ItemCount from './components/ItemCount/ItemCount';
 import Promesas from './components/Promesas/Promesas';
+import Fetch from './components/Fetch/Fetch';
 
 
 function App() {
   return (
     <>
       <NavBar />
-      <ItemListContainer greeting="Hola Mundo" />
+      <ItemListContainer greeting="Productos" />
       {
         //<ItemCount />
-
+        //<Promesas/>
+        //<Fetch/>
       }
-      <Promesas/>
+      
       
     </>
   );
